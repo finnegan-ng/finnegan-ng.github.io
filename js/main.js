521d33ed024e529e5c66d80f4b26401d7dec90a9
finnegan-ng <finnegan.ng@mail.com>
2020-12-20 12:52:06 -0500

    commit

--- js/main.js
+++ js/main.js
@@ -254,7 +254,7 @@
 		      error: function() {
 
 		      	sLoader.fadeOut();
-		      	$('#message-warning').html("Something went wrong. Please try again.");
+		      	$('#message-warning').html("JSSomething went wrong. Please try again.");
 		         $('#message-warning').fadeIn();
 
 		      }
